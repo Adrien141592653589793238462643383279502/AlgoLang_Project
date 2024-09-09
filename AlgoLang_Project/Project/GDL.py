5ce5b47dd8273a90438b27e84084758940c1e6cf
tantrax13 <126810297+tantrax13@users.noreply.github.com>
2024-09-09 10:58:24 +0200

    Update GDL.py
    ajout du choix que deux lettre ne peuvent pas se suivre

--- AlgoLang_Project/Project/GDL.py
+++ AlgoLang_Project/Project/GDL.py
@@ -4,6 +4,13 @@ file_adjs = open("liste_adjs.txt", "r")
 result = open("result.txt", "w")
 char_min = int(input('Minimum number of characters : '))
 char_max = int(input('Maximum number of characters : '))
+char_rep = input("is the repetition of two letters authorised? yes/no : ")
+fr_add_L = ""
+new_L = ""
+if char_rep == "yes": 
+    char_rep = True
+else : 
+    char_rep = False
 unique_words = set()
 
 word =""
@@ -19,8 +26,18 @@ for line in file_adjs:
     letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     
     while True:
-        word_t = ''.join(choice(letter) for _ in range(num_char))
-        
+        for _ in range (num_char):
+            while True : 
+                new_L = choice(letter)
+                if new_L != fr_add_L and char_rep == False :
+                    fr_add_L = new_L
+                    word_t = word_t + new_L
+                    break
+                if char_rep == True :
+                    word_t = word_t + new_L
+                    break
+            new_L = ""    
+
         if word_t not in unique_words:
             unique_words.add(word_t)
             break
