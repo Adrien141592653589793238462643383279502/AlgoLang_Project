e24b8a3e06da25e5b4b212557518aff18ffe048a
Sacha <sacha.mekkaoui@gmail.com>
2024-09-09 23:05:34 +0200

    Update GDL.py

--- AlgoLang_Project/Project/GDL.py
+++ AlgoLang_Project/Project/GDL.py
@@ -22,7 +22,7 @@ for line in file_adjs:
     num_char = randint(char_min,char_max)
     print(num_char)
     
-    i = 0
+    
     letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     
     while True:
