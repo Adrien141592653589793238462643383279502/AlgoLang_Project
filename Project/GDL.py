d69a877ef066897cb8a6083798e8dadd7c8fae79
Sacha <sacha.mekkaoui@gmail.com>
2024-09-15 11:45:53 +0200

    Update GDL.py

--- Project/GDL.py
+++ Project/GDL.py
@@ -35,3 +35,73 @@ def generate_language(char_min, char_max, char_rep):
                     unique_words.add(word_t)
                     break
             result.write(f"{word} : {word_t}\n")
+    
+
+    with open("liste_noms.txt", "r") as file_noms, open("algo.nom.txt", "w") as result:
+        fr_add_L = ""
+        new_L = ""
+        word = ""
+        word_t = ""
+        num_char = 0
+
+        for line in file_noms:
+            word = line.strip()
+            num_char = randint(char_min, char_max)
+            
+            letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+            
+            while True:
+                word_t = ""
+                fr_add_L = ""
+
+                for _ in range(num_char):
+                    while True:
+                        new_L = choice(letter)
+                        
+                        if char_rep == "no" and new_L == fr_add_L:
+                            continue
+                        else:
+                            break
+
+                    word_t += new_L
+                    fr_add_L = new_L 
+
+                if word_t not in unique_words:
+                    unique_words.add(word_t)
+                    break
+            result.write(f"{word} : {word_t}\n")
+
+
+    with open("liste_vbs.txt", "r") as file_vbs, open("algo.vbs.txt", "w") as result:
+        fr_add_L = ""
+        new_L = ""
+        word = ""
+        word_t = ""
+        num_char = 0
+
+        for line in file_vbs:
+            word = line.strip()
+            num_char = randint(char_min, char_max)
+            
+            letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+            
+            while True:
+                word_t = ""
+                fr_add_L = ""
+
+                for _ in range(num_char):
+                    while True:
+                        new_L = choice(letter)
+                        
+                        if char_rep == "no" and new_L == fr_add_L:
+                            continue
+                        else:
+                            break
+
+                    word_t += new_L
+                    fr_add_L = new_L 
+
+                if word_t not in unique_words:
+                    unique_words.add(word_t)
+                    break
+            result.write(f"{word} : {word_t}\n")
