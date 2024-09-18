2f075984c5e519bc3f27453d1136e9de774e646d
Sacha <sacha.mekkaoui@gmail.com>
2024-09-18 19:16:45 +0200

    Update GDL.py

--- Project/GDL.py
+++ Project/GDL.py
@@ -1,15 +1,15 @@
 from random import choice, randint
 
-def generate_language(char_min, char_max, char_rep):
-    with open("list_adjs.txt", "r") as file_adjs, open("algo.adj.txt", "w") as result:
-        fr_add_L = ""
-        new_L = ""
+def generate_language(char_min, char_max, char_rep, cons_rep, voy_rep):
+    with open("liste_mots.txt", "r") as file_mots, open("algo.mots.txt", "w") as result:
+        
+        
         unique_words = set()
         word = ""
         word_t = ""
         num_char = 0
 
-        for line in file_adjs:
+        for line in file_mots:
             word = line.strip()
             num_char = randint(char_min, char_max)
             
@@ -23,9 +23,9 @@ def generate_language(char_min, char_max, char_rep):
                 for _ in range(num_char):
                     while True:
                         new_L = choice(letter)
-                        if new_L in consonne and fr_add_L in consonne and fr_add_L2 in consonne:
+                        if new_L in consonne and fr_add_L in consonne and fr_add_L2 in consonne and cons_rep == "no":
                             new_L = choice(voyelle)
-                        if new_L in voyelle and fr_add_L in voyelle and fr_add_L2 in voyelle:
+                        if new_L in voyelle and fr_add_L in voyelle and fr_add_L2 in voyelle and voy_rep == "no":
                             new_L = choice(consonne)
                         elif char_rep == "no" and new_L == fr_add_L:
                             continue
@@ -42,79 +42,3 @@ def generate_language(char_min, char_max, char_rep):
             result.write(f"{word} : {word_t}\n")
     
 
-    with open("list_nouns.txt", "r") as file_noms, open("algo.nom.txt", "w") as result:
-        fr_add_L = ""
-        new_L = ""
-        word = ""
-        word_t = ""
-        num_char = 0
-
-        for line in file_noms:
-            word = line.strip()
-            num_char = randint(char_min, char_max)
-            
-            
-
-            while True:
-                word_t = ""
-                fr_add_L = ""
-                fr_add_L2 = ""
-                for _ in range(num_char):
-                    while True:
-                        new_L = choice(letter)
-                        if new_L in consonne and fr_add_L in consonne and fr_add_L2 in consonne:
-                            new_L = choice(voyelle)
-                        if new_L in voyelle and fr_add_L in voyelle and fr_add_L2 in voyelle:
-                            new_L = choice(consonne)
-                        elif char_rep == "no" and new_L == fr_add_L:
-                            continue
-                        else:
-                            break
-
-                    word_t += new_L
-                    fr_add_L2 = fr_add_L
-                    fr_add_L = new_L 
-                    
-                if word_t not in unique_words:
-                    unique_words.add(word_t)
-                    break
-            result.write(f"{word} : {word_t}\n")
-
-
-    with open("list_vbs.txt", "r") as file_vbs, open("algo.vbs.txt", "w") as result:
-        fr_add_L = ""
-        new_L = ""
-        word = ""
-        word_t = ""
-        num_char = 0
-
-        for line in file_vbs:
-            word = line.strip()
-            num_char = randint(char_min, char_max)
-            
-            
-            
-            while True:
-                word_t = ""
-                fr_add_L = ""
-                fr_add_L2 = ""
-                for _ in range(num_char):
-                    while True:
-                        new_L = choice(letter)
-                        if new_L in consonne and fr_add_L in consonne and fr_add_L2 in consonne:
-                            new_L = choice(voyelle)
-                        if new_L in voyelle and fr_add_L in voyelle and fr_add_L2 in voyelle:
-                            new_L = choice(consonne)
-                        elif char_rep == "no" and new_L == fr_add_L:
-                            continue
-                        else:
-                            break
-
-                    word_t += new_L
-                    fr_add_L2 = fr_add_L
-                    fr_add_L = new_L 
-
-                if word_t not in unique_words:
-                    unique_words.add(word_t)
-                    break
-            result.write(f"{word} : {word_t}\n")
