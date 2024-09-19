3b1d67ce1ca18e5e761cc29b3bdf9e280b4b28fe
Sacha <sacha.mekkaoui@gmail.com>
2024-09-19 19:50:51 +0200

    Update GDL.py

--- Project/GDL.py
+++ Project/GDL.py
@@ -18,6 +18,7 @@ def melanger():
 
 
 def generate_language(char_min, char_max, char_rep, cons_rep, voy_rep):
+    
     with open("liste_mots.txt", "r") as file_mots, open("algo.mots.txt", "w") as result:
     
         unique_words = set()
@@ -32,32 +33,36 @@ def generate_language(char_min, char_max, char_rep, cons_rep, voy_rep):
             letter = ["a","b","c","d","e","é","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
             consonne = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z"]
             voyelle = ["a","e","é","i","o","u","y"]
+            
             while True:
                 word_t = ""
                 fr_add_L = ""
                 fr_add_L2 = ""
-                if num_char == 1:
-                    word_t = choice(letter)
-                    if word_t in unique_words:
-                        num_char += randint(char_min, char_max - 1)
-                else:
-                    for _ in range(num_char):
-                        while True:
-                            new_L = choice(letter)
-                            if new_L in consonne and fr_add_L in consonne and fr_add_L2 in consonne and cons_rep == "no":
-                                new_L = choice(voyelle)
-                            if new_L in voyelle and fr_add_L in voyelle and fr_add_L2 in voyelle and voy_rep == "no":
-                                new_L = choice(consonne)
-                            elif char_rep == "no" and new_L == fr_add_L:
-                                continue
-                            else:
-                                break
+                
+                for _ in range(num_char):
+                    
+                    while True:
+                        
+                        new_L = choice(letter)
+                        
+                        if new_L in consonne and fr_add_L in consonne and fr_add_L2 in consonne and cons_rep == "no":
+                            new_L = choice(voyelle)
+                            
+                        if new_L in voyelle and fr_add_L in voyelle and fr_add_L2 in voyelle and voy_rep == "no":
+                            new_L = choice(consonne)
+                            
+                        elif char_rep == "no" and new_L == fr_add_L:
+                            continue
+                            
+                        else:
+                            break
 
-                        word_t += new_L
-                        fr_add_L2 = fr_add_L
-                        fr_add_L = new_L 
+                    word_t += new_L
+                    fr_add_L2 = fr_add_L
+                    fr_add_L = new_L 
 
                 if word_t not in unique_words:
                     unique_words.add(word_t)
                     break
+                    
             result.write(f"{word} : {word_t}\n")
