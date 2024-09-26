c94a29375f02ff6d26f70ec53a0ccf4411e7886a
Sacha <sacha.mekkaoui@gmail.com>
2024-09-26 19:14:01 +0200

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+
+list_words = open("algo_words_final.txt", "r")
+
+while True:
+    word = input("Quel mot cherchez-vous? Ecrivez stop pour arrêter le programme \n")
+    for line in list_words:
+        word_s = line.strip()
+        if word in word_s:
+            print(word_s)
+            break
+        else:
+            print("Le mot n'est pas dans la liste")
+    if word == "stop":
+        break
